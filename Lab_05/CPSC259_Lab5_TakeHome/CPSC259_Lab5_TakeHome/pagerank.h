adc7279d31b789a1e9f84ace2f11ba76bc9cafdd
liamfoster77 <liamfoster77@gmail.com>
2021-11-30 13:51:58 -0800

    Made new framework

---
+++ Lab_05/CPSC259_Lab5_TakeHome/CPSC259_Lab5_TakeHome/pagerank.h
@@ -0,0 +1,22 @@
+/*
+ File:          websolver.h
+ Purpose:       Contains constants, prototypes, globals
+ Author:			  Your names
+ Student #s:		12345678 and 12345678
+ CWLs:      		cwl1 and cwl2
+ Date:				  Add the date here
+ */
+
+
+#pragma once
+
+  /* Preprocessor directives to define macros */
+#define WEB1     "web.txt"
+#define BUFFER    128
+
+#include <stdio.h>
+#include <stdlib.h>
+
+                /* Function prototypes */
+int          get_web_dimension(FILE* web_file);
+int** parse_web(FILE* web_file, int dimension);
